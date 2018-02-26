3b968dcb1d745247d8d220f8f0e8dc86f7a6ef10
luloop <36158159+luloop@users.noreply.github.com>
2018-02-26 03:42:57 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
+var numero = parseInt (prompt ( "ingrese un numero"));
+for ( var i = 2; i <= numero; i++)
+{
+    if ( numero % i == 0 && i != numero)
+    { 
+    alert ("su numero NO es primo");
+    break;
+    }
+    if (i % numero == 0 )
+    {
+         alert( " su numero es primo");
+    }
 
+
+}
 
 
 
